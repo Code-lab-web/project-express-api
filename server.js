eae8625ad9b6971ff32ed69ffb49800052f42d9c
Johan Skarpsvärd <145796632+Code-lab-web@users.noreply.github.com>
2025-07-08 18:59:29 +0000

    git commit -m "Code-lab-web"

--- server.js
+++ server.js
@@ -4,6 +4,37 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import crypto from "crypto";
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt-nodejs'
+
+const mongoUrl =process.env_MONGO_URL II "mongodb://localhost/auth"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useundefinedtopology: true})
+mongoose.Promise = Promise
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        unique: true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    accessToken:{
+        type:String,
+        default: () => crypto.randomBytes(128)toString('hex')
+    }
+    });
+// Example
+    // POST Request
+    const request = {name: "Bob", password: "foobar};
+        // DB Entry
+        const dbEntry = {name :"Bob, password:"5abbc32983def"}
+            bcrypt.compareSync(request.password, dbEntry.password);
+             // One-way encryption
+const user = new User({name:"Bob", password:bcrypt.hashSync("foobar")});
+user.save();
+// Defines the port the app will run on. Defaults to 8080, but can be overritten when starting the server. For example:
+//
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
@@ -44,10 +75,6 @@ app.get("/", (req, res) => {
 // This will only happen if the next() function is called from middleware!
         // now we can access the req.user object from the middleware
     })
-    app.post('/sessions', assync (req, res) => {
-        const user = await UserActivation.findOne({name: req.body.name});
-        if (user && bcrypt.compareSync(req.body.password, userpassword)) {
-            // Success
         }
     })
 
@@ -69,9 +96,9 @@ app.post('/sessions' async (req res) => {
     }
 });
 // Start the server
-})app.get('/,(req, res) => {
-    res.send('Hello world')
 })
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
+})
+console.log(bcrypt.hashSync("foobar"));
 });
